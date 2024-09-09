c2fd9ec6db8e984807be3582e886e38bd7aae6b1
Henry Tottle <henrywilliamtottle@gmail.com>
2024-09-09 20:54:25 +0100

    populate DB php

---
+++ furnitureStoreDBPopulate.php
@@ -0,0 +1,35 @@
+<?php
+require('vendor/autoload.php');
+$jsonRequest = file_get_contents('https://dev.io-academy.uk/resources/furniture/furniture.json');
+$json = json_decode($jsonRequest, true);//This needs to be set to true for the $query below to work.
+//It threw error saying it couldn't use stdType Class.
+try
+{
+    $db = new PDO('mysql:host=db;dbname=furniture_store', 'root', 'password');
+}
+catch (Exception $exception)
+{
+    $logPath = 'error.log';
+    $errorMessage = PHP_EOL.$exception->getMessage();
+    file_put_contents($logPath,$errorMessage, FILE_APPEND);
+    echo '<h1>Database Connection Error, look at error.log</h1>';
+}
+//Important note: I have changed price to a string as we haven't removed £ yet.  Maybe that is a task for later on?
+foreach ($json as $eachJson)
+{
+    var_dump($eachJson);
+    echo '<br>';
+    $query = $db->prepare(
+        'INSERT INTO `products` (`name`, `width`, `height`, `depth`, `price`, `stock`, `related`, `color`)
+                                 VALUES (:name, :width,:height,:depth,
+                                         :price,:stock,:related,:color)');
+    $query->execute(['name' =>$eachJson['name'],
+        'width'=>$eachJson['width'],
+        'height'=>$eachJson['height'],
+        'depth'=>$eachJson['depth'],
+        'price'=>$eachJson['price'],
+        'stock'=>$eachJson['stock'],
+        'related'=>$eachJson['related'],
+        'color'=>$eachJson['color']]);
+}
+//HAS BEEN RUN, DO NOT RUN AGAIN
