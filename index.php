e7152420eefc6da5a029d03771db5478ee88703c
Henry Tottle <henrywilliamtottle@gmail.com>
2024-09-09 16:09:08 +0100

    peer review changes

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
 require('vendor/autoload.php');
-
-
-
 $jsonRequest = file_get_contents('https://dev.io-academy.uk/resources/furniture/furniture.json');
 $json = json_decode($jsonRequest);
-
-//var_dump($json);
 
 try
 {
