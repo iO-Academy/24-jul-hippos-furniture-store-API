ea75ddf0b53f185e57198eebac520632c7559d3a
Henry Tottle <henrywilliamtottle@gmail.com>
2024-09-09 16:51:34 +0100

    starting DB connect

--- index.php
+++ index.php
@@ -15,3 +15,15 @@ catch (Exception $exception)
     echo '<h1>Database Connection Error, look at error.log</h1>';
 }
 
+//print_r($json);
+
+foreach ($json as $eachJson)
+{
+    var_dump($eachJson);
+    echo '<br>';
+    $query = $db->prepare(
+        'INSERT INTO `products` (`name`, `width`, `height`, `depth`, `price`, `stock`, `related`, `colour`)
+                                 VALUES (:name, :width,:height,:depth,
+                                         :price,:stock,:related,:colour)');
+    $query->execute(['name' =>$eachJson['name'], ]);
+}
