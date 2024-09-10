45ede5549d62fd32820077c72957bf91b63da524
Henry Tottle <henrywilliamtottle@gmail.com>
2024-09-10 11:10:56 +0100

    minor change to property  name

--- src/Products/DetailedProductEntity.php
+++ src/Products/DetailedProductEntity.php
@@ -4,7 +4,7 @@ namespace FurnitureStoreApi\Products;
 use FurnitureStoreApi\Products\SimpleProductEntity as SimpleProductEntity;
 class DetailedProductEntity extends SimpleProductEntity
 {
-    private int $category_id;
+    private int $cid;
     private int $width;
     private int $height;
     private int $depth;
