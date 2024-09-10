aa9ddec75a4182caf9cfbf1cbc10419e48a86f54
Joseph Padfield <joseph.padfield@gmail.com>
2024-09-10 15:20:31 +0100

    added category_id col to products table. changed category hydrator to reflect this change

--- src/Categories/CategoryHydrator.php
+++ src/Categories/CategoryHydrator.php
@@ -7,12 +7,13 @@ class CategoryHydrator
     public static function getCategories()
     {
         $db = new PDO('mysql:host=db;dbname=furniture_store', 'root', 'password');
-        $query = $db->prepare('SELECT `categories`.`id`, `category`, COUNT(`categories`.`id`) AS `category_id` FROM `categories`
-                            JOIN `products` ON `products`.`name`= `categories`.`category`
-                            GROUP BY `category`
-                            ORDER BY `categories`.`id`');
+        $query = $db->prepare('SELECT `id`, `name`, COUNT(`category_id`) AS `category_id` FROM `products`
+                            GROUP BY `category_id`
+                            ORDER BY `category_id`');
         $query->execute();
         $query->setFetchMode(PDO::FETCH_CLASS, CategoryEntity::class);
         return $query->fetchAll();
     }
 }
+
+
