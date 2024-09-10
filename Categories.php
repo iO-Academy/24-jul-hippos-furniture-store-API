1d9566c72b7008517cb2006a5de4716b02a4347e
Joseph Padfield <joseph.padfield@gmail.com>
2024-09-10 11:01:53 +0100

    completed get categories

--- Categories.php
+++ Categories.php
@@ -1,22 +1,8 @@
 <?php
+require_once ('vendor/autoload.php');
 
-use FurnitureStoreApi\Categories\CategoryHydrator;
-use \PDO;
+use \FurnitureStoreApi\Categories\CategoryHydrator;
 
-try {
+$result = CategoryHydrator::getCategories();
 
-    $db = new PDO('mysql:host=db;dbname=furnitureStoreProject', 'root', 'password');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    $query = $db->prepare('SELECT `categories`.`id`, `category`, COUNT(`categories`.`id`) FROM `categories`
-                            JOIN `products` ON `products`.`name`= `categories`.`category`
-                            GROUP BY `category`
-                            ORDER BY `categories`.`id`');
-    $query->execute();
 
-} catch (Exception $exception) {
-    $logPath = 'error.log';
-    $errorMessage = PHP_EOL . $exception->getMessage();
-    file_put_contents($logPath, $errorMessage, FILE_APPEND);
-    echo '<h1>Database Connection Error, look at error.log</h1>';
-}
